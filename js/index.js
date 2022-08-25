dcec964afb961663bf7fa86890800f72bb4398f9
Merry Rosalie <z5391208@ad.unsw.edu.au>
2022-08-25 20:33:32 +1000

    Enable lazy loading and make scrollspy responsive

--- js/index.js
+++ js/index.js
@@ -10,37 +10,22 @@ window.onload = () => {
 	$('#bongo-cat').on('touchstart', pawsDown);
 	$('#bongo-cat').on('touchend', pawsUp);
 
-	$('.about-link').on("click", (element) => {
-		element.preventDefault();
-		$('html, body').animate({
-			scrollTop: $("#About").offset().top - 50
-		}, 750);
-	});
-	
-	$('.projects-link').on("click", (element) => {
-		element.preventDefault();
-		$('html, body').animate({
-			scrollTop: $("#Projects").offset().top - 50
-		}, 750);
-	});
-	
-	$('.contact-link').on("click", (element) => {
-		element.preventDefault();
-		$('html, body').animate({
-			scrollTop: $("#Contact").offset().top - 50
-		}, 750);
-	});
-
 	setInterval(() => {
 		let src = $('.avatar').attr("src");
 		let dict = {
-			'../static/front.png': '../static/right.png',
-			'../static/right.png': '../static/back.png',
-			'../static/back.png': '../static/left.png',
-			'../static/left.png': '../static/front.png',
+			'static/front.png': 'static/right.png',
+			'static/right.png': 'static/back.png',
+			'static/back.png': 'static/left.png',
+			'static/left.png': 'static/front.png',
 		}
 		$('.avatar').attr("src", dict[src]);
 	}, 1500);
+
+	$('.about-link').on("click", (element) => scrollspy(element, '#About'));
+	
+	$('.projects-link').on("click", (element) => scrollspy(element, '#Projects'));
+	
+	$('.contact-link').on("click", (element) => scrollspy(element, '#Contact'));
 };
 
 function pawsDown() {
@@ -52,3 +37,10 @@ function pawsUp() {
 	$('.funni-bongo-cat').addClass('paws-up');
 	$('.funni-bongo-cat').removeClass('paws-down');
 };
+
+function scrollspy(element, target) {
+	element.preventDefault();
+	$('html, body').animate({
+		scrollTop: $(target).offset().top - $('.nav').height() - 50
+	}, 750);
+}
